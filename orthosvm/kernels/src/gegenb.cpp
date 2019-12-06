dde4b3b603962fec4955acdc3762c9306a1f0168
Edwin <developeredwin@gmail.com>
2019-12-06 16:30:48 -0600

    :bug:
    Bug fixed, the problem was that the multiplication element is one and not zero

--- orthosvm/kernels/src/gegenb.cpp
+++ orthosvm/kernels/src/gegenb.cpp
@@ -5,7 +5,7 @@ double pochhammer(double x, int n)
     if (n == 0) return 1.0;
     else if (n < 0 || x == 0) return 0.0;
 
-    double result = 0.0;
+    double result = 1.0;
 
     for (int i = 0; i < n; i++)
     {
