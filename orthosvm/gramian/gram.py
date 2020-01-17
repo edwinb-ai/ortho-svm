ecc3941211b7d3a5ece09534e81001e3b634c9e3
Edwin <developeredwin@gmail.com>
2020-01-16 18:19:04 -0600

    :sparkles: :white_check_mark:
    The Gram class now has every implementation necessary, new tests are being developed

--- orthosvm/gramian/gram.py
+++ orthosvm/gramian/gram.py
@@ -1,10 +1,16 @@
 import numpy as np
 from sklearn.utils import check_X_y
+from orthosvm.kernels import hermite, gegenbauer
 
 
 class Gram:
     def __init__(self, X, **kwargs):
         self.x = X
+        self.y = None
+        # self.kernel_type = None
+        # self.degree = None
+        # self.alpha = None
+        # self.gamma = None
 
         if "y" in kwargs:
             self.y = kwargs["y"]
@@ -19,9 +25,47 @@ class Gram:
         if "gamma" in kwargs:
             self.gamma = kwargs["gamma"]
 
+    @property
+    def gram(self):
+        return self._compute_gram_matrix(self.x, y=self.y)
+
     def _check_arrays(self):
         self.x, self.y = check_X_y(self.x, self.y)
 
-    # TODO: Put the computation of the gram matrix here!
-    def kernel(self):
-        pass
+    def _give_kernel(self, x, z):
+        if self.kernel_type == "hermite":
+            return hermite.kernel(x, z, self.degree)
+        if self.kernel_type == "gegenbauer":
+            return gegenbauer.kernel(x, z, self.degree, self.alpha)
+
+    def _iterate_over_arrays(self, X, y, gram):
+        for l, x in enumerate(X):
+            for m, z in enumerate(y):
+                summ, mult = 1.0, 1.0
+                for i, k in zip(x, z):
+                    summ = 1.0
+                    if i != 0.0 and k != 0.0:
+                        summ = self._give_kernel(i, k)
+                    mult *= summ
+                    # * The matrix will be symmetric
+                    if y is not None:
+                        gram[l, m] = gram[m, l] = mult
+                        if m > l:
+                            break
+                    # * The matrix won't be symmetric
+                    else:
+                        gram[l, m] = mult
+        return np.copy(gram)
+
+    def _compute_gram_matrix(self, X, y=None):
+        if y is not None:
+            X_gram = np.zeros((X.shape[0], y.shape[0]))
+            X_gram = self._iterate_over_arrays(X, y, X_gram)
+        else:
+            X_gram = np.zeros((X.shape[0], X.shape[0]))
+            X_gram = self._iterate_over_arrays(X, X, X_gram)
+
+        return X_gram
+
+    def gram_matrix(self):
+        return self._compute_gram_matrix
--- tests/test_gram_class.py
+++ tests/test_gram_class.py
@@ -1,21 +1,21 @@
 from orthosvm.gramian import gram
 import numpy as np
 import pytest
-from orthosvm.kernels import hermite
 from orthosvm.gramian import compute_gram_matrix
+from sklearn.svm import SVC
 
 
 def test_gram_arrays():
     """Should pass the test because arrays are the same size
     """
-    gram_matrix = gram.Gram(np.zeros((5, 5)), np.zeros(5), "linear")
+    gram_matrix = gram.Gram(np.zeros((5, 5)), y=np.zeros(5), kernel="linear")
 
 
 def test_gram_arrays_fail():
     """When the arrays are of wrong size, raise an error
     """
     with pytest.raises(ValueError):
-        gram_matrix = gram.Gram(np.zeros((5, 5)), np.zeros(10), "linear")
+        gram_matrix = gram.Gram(np.zeros((5, 5)), y=np.zeros(10), kernel="linear")
 
 
 def load_and_strip(path):
@@ -23,14 +23,19 @@ def load_and_strip(path):
     return dataset[..., 1:]
 
 
-def test_decorator():
-    fourclass = np.loadtxt("tests/datasets/fourclass.csv", delimiter=",", skiprows=1)
-    X = fourclass[:, :2]
+fourclass = np.loadtxt("tests/datasets/fourclass.csv", delimiter=",", skiprows=1)
+X = fourclass[:, :2]
 
-    common_path = "tests/datasets/"
-    expected_results = load_and_strip(common_path + "hermite_gramian_fourclass.csv")
+common_path = "tests/datasets/"
+expected_results = load_and_strip(common_path + "hermite_gramian_fourclass.csv")
 
-    assert pytest.approx(
-        compute_gram_matrix(X, kernel=hermite.kernel, degree=6) == expected_results,
-        rel=1e-15,
-    )
+gram_matrix = gram.Gram(X, kernel="hermite", degree=6)
+
+
+def test_gram_object_attributes():
+    assert pytest.approx(gram_matrix.gram == expected_results, rel=1e-15)
+
+
+def test_sklearn_integration():
+    gram_matrix = gram.Gram(X, kernel="hermite", degree=6)
+    svc = SVC(kernel=gram_matrix.gram_matrix())
